02d723928e44660047c565b4810ddf287a2533fc
autodl <1161057855@qq.com>
2024-12-05 15:43:28 +0800

    passkey.sh

--- scripts/passkey.sh
+++ scripts/passkey.sh
@@ -1,19 +1,22 @@
 cd evaluation/passkey
 
-MODEL=Llama-3.2-1B-Instruct
-#Llama-3.1-8B-Instruct
-MODELPATH=meta-llama/Llama-3.2-1B-Instruct
-#meta-llama/Llama-3.1-8B-Instruct
-OUTPUT_DIR=results/$MODEL
+MODEL=Llama-3.1-8B-Instruct
+#Llama-3.2-1B-Instruct
+MODELPATH=meta-llama/Llama-3.1-8B-Instruct
+#meta-llama/Llama-3.2-1B-Instruct
+OUTPUT_DIR=results/$MODEL/amax
 
 mkdir -p $OUTPUT_DIR
 
 length=100000
 
-for token_budget in 512 1024 2048 4096
+for token_budget in 256 512 1024 2048 4096
 do
     python passkey.py -m $MODELPATH \
         --iterations 100 --fixed-length $length \
         --quest --token_budget $token_budget --chunk_size 16 \
         --output-file $OUTPUT_DIR/$MODEL-quest-$token_budget.jsonl
 done
+
+cd ..
+python get_repo_branch.py --model_path $MODELPATH
