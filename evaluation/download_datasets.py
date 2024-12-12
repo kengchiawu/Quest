e16cec5f8f7430805968450c9c9cd80e46811c04
199u <1161057855@qq.com>
2024-12-12 12:36:47 +0800

    create h2o for ppl_eval

---
+++ evaluation/download_datasets.py
@@ -0,0 +1,3 @@
+from datasets import load_dataset
+
+data = load_dataset("emozilla/pg19-test", split="test")
--- evaluation/pg19/ppl_eval.py
+++ evaluation/pg19/ppl_eval.py
@@ -3,8 +3,8 @@ from tqdm import tqdm
 import os
 os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 #os.environ['HF_HOME'] = '/root/autodl-tmp/cache/'
-os.environ['CUDA_VISIBLE_DEVICES'] = '3,4'
-from transformers import AutoModelForCausalLM, AutoTokenizer
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from datasets import load_dataset
 from torch.nn import CrossEntropyLoss
 
@@ -12,6 +12,7 @@ import argparse
 from argparse import ArgumentParser
 
 device = "cuda"
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", action="append", nargs="+")
@@ -25,7 +26,11 @@ parser.add_argument("--iterations", type=int, default=20)
 parser.add_argument("--output_dir", type=str)
 
 parser.add_argument("--num_eval_tokens", type=int, default=None)
-parser.add_argument('--enable_h20', action='store_true',help="Enable heavy-hitter attention")
+#h2o parameters
+parser.add_argument('--h2o', action='store_true',help="Enable heavy-hitter attention")
+parser.add_argument("--heavy_ratio", type=float, default=0.1)
+parser.add_argument("--recent_ratio", type=float, default=0.1)
+#quest parameters
 parser.add_argument("--quest", action="store_true", help="Enable quest attention")
 parser.add_argument("--token_budget", type=int, default=1024)
 parser.add_argument("--chunk_size", type=int, default=16)
@@ -33,7 +38,18 @@ parser.add_argument("--chunk_size", type=int, default=16)
 
 def load(model_name_or_path):
     print(f"Loading model from {model_name_or_path} ...")
+
+    #rope_scaling_config = {
+    #    "type": "llama3",  # 或者其他支持的类型
+    #    "factor": 32.0     # 这里使用 factor 作为缩放因子
+    #}
+
     # however, tensor parallel for running falcon will occur bugs
+    config = AutoConfig.from_pretrained(
+        model_name_or_path,
+        #cache_dir=args.cache_dir,
+        #rope_scaling=rope_scaling_config,
+        )
     tokenizer = AutoTokenizer.from_pretrained(
         model_name_or_path,
         trust_remote_code=True,
@@ -52,14 +68,14 @@ def load(model_name_or_path):
 
     model.eval()
 
-    return model, tokenizer
+    return model, tokenizer,config
 
 
 args = parser.parse_args()
 
 data = load_dataset("emozilla/pg19-test", split="test")
 
-model, tokenizer = load(args.model_name_or_path)
+model, tokenizer,config = load(args.model_name_or_path)
 
 nlls = []
 loss_fn = CrossEntropyLoss(reduction="none")
@@ -72,6 +88,18 @@ if args.quest:
     )
 
     enable_quest_attention_eval(model, args)
+
+if args.h2o:
+    print('Enable heavy hitter')
+    from evaluation.h2o_modify_llama import(
+        convert_kvcache_llama_heavy_recent,
+    )
+    config.heavy_ratio = args.heavy_ratio
+    config.recent_ratio = args.recent_ratio
+    #checkpoint = copy.deepcopy(model.state_dict())
+    model = convert_kvcache_llama_heavy_recent(model, config)
+    #model.load_state_dict(checkpoint)
+    model.half().eval().cuda()
 
 os.makedirs(args.output_dir, exist_ok=True)
 f = open(f"{args.output_dir}/log.txt", "w")
