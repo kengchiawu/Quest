2ac2374ddfab0d5f6b3f72da6fe0726b02737e6c
199u <1161057855@qq.com>
2024-12-24 19:58:09 +0800

    use llama-3.2-1B in ppl

--- scripts/ppl_eval.sh
+++ scripts/ppl_eval.sh
@@ -1,17 +1,18 @@
 cd evaluation/pg19
 
-MODELPATH=lmsys/longchat-7b-v1.5-32k
-OUTPUT_DIR=results/ppl_eval/longchat/amax
+MODELPATH=meta-llama/Llama-3.1-8B-Instruct
+#lmsys/longchat-7b-v1.5-32k
+OUTPUT_DIR=results/ppl_eval/Llama-3.1-8B-Instruct/amax
 mkdir -p $OUTPUT_DIR
 
-device=0
+#device=0
 budget=4096
 
 CUDA_VISIBLE_DEVICES=0,1,2 python -u ppl_eval.py \
     --model_name_or_path $MODELPATH \
     --output_dir $OUTPUT_DIR \
-    --num_eval_tokens 30000 \
-    --quest --token_budget $budget --chunk_size 16 
+    --num_eval_tokens 6000 \
+    #--quest --token_budget $budget --chunk_size 16 
     
-cd ..
-python get_repo_branch.py --model_path $MODELPATH
+#cd ..
+#python get_repo_branch.py --model_path $MODELPATH
